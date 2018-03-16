41903ab01f8b8b94cd30de8a0b94f3b047b98bdb
Soirat Nicolas <nicolas.soirat@etu.umontpellier.fr>
2018-03-16 18:12:52 +0100

    Restart à 0 suite au bug du push...

--- fasta.cpp
+++ fasta.cpp
@@ -1,1 +1,65 @@
 #include "fasta.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+/*
+================================================================================
+=                                 Constructeurs                                =
+================================================================================
+*/
+Fasta::Fasta(){}
+Fasta::Fasta(string header, string sequence, int longueur, int position){
+  this->header=header;
+  this->sequence=sequence;
+  this->longueur=longueur;
+  this->position=position;
+}
+
+/*
+================================================================================
+=                                 Accesseurs                                   =
+================================================================================
+*/
+//GET
+string getHeader(){
+  return header;
+}
+string getSequence(){
+  return sequence;
+}
+int getLongueur(){
+  return longueur;
+}
+int getPos(){
+  return position;
+}
+//SET
+void setHeader(string header){
+  this->header=header;
+}
+void setSequence(string sequence){
+  this->sequence=sequence;
+}
+void setPos(int position){
+  this->position=position;
+}
+void setLongueur(int longueur){
+  this->position=position;
+}
+
+/*
+================================================================================
+=                                 Méthodes                                     =
+================================================================================
+*/
+
+//Verifications
+bool isFasta() const{}
+bool isNucl(char c, bool degenerate=TRUE){}
+bool isBlank(char c);
+
+//Autres
--- fasta.h
+++ fasta.h
@@ -2,16 +2,67 @@
 #define __fasta__
 
 #include <iostream>
+#include <vector>
 
 
-class fasta {
+class Fasta {
+
+/*
+================================================================================
+=                                 Variables                                    =
+================================================================================
+*/
+
 private:
-  std::string seq;
-  std::int longueur;
-  std::int position;
+  std::string header;
+  std::string sequence;
+  size_t longueur;
+  size_t position;
+  std::vector<std::string>list_sequence;
+  std::vector<std::string>list_header;
+  std::vector<size_t>list_longueur;
+  std::vector<size_t>list_position;
 public:
+
+/*
+================================================================================
+=                                 Constructeurs                                =
+================================================================================
+*/
   fasta();
-  fasta(std::string seq, std::int longueur, std::int position);
+  fasta(std::string header, std::string sequence, size_t longueur, size_t position);
+
+/*
+================================================================================
+=                                 Accesseurs                                   =
+================================================================================
+*/
+  //GET
+  std::string getHeader() const;
+  std::string getSequence() const;
+  size_t getLongueur() const;
+  size_t getPos() const;
+  //SET
+  void setHeader(string header);
+  void setSequence(string sequence);
+  void setLongueur(int longueur);
+  void setPos(int pos);
+
+/*
+================================================================================
+=                                 Méthodes                                     =
+================================================================================
+*/
+
+  //Verifications
+  bool isFasta() const;
+  bool isNucl(char c, bool degenerate=TRUE) const;
+  bool isBlank(char c) const;
+
+  //Autres
+
+
+
 };
 
 #endif
