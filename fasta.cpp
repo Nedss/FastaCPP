84d9de684bc35bb9d6f24d67fff4321a920dd7ff
Nedss <34100544+Nedss@users.noreply.github.com>
2018-03-20 01:44:48 +0100

    Préparation importation brouillon

--- fasta.cpp
+++ fasta.cpp
@@ -7,17 +7,19 @@
 
 using namespace std;
 
+class Fasta{
+
 /*
 ================================================================================
 =                                 Constructeurs                                =
 ================================================================================
 */
-Fasta::Fasta(){}
-Fasta::Fasta(string header, string sequence, int longueur, int position){
-  this->header=header;
-  this->sequence=sequence;
-  this->longueur=longueur;
-  this->position=position;
+  Fasta::Fasta(){}
+  Fasta::Fasta(string header, string sequence, int longueur, int position){
+    this->header=header;
+    this->sequence=sequence;
+    this->longueur=longueur;
+    this->position=position;
 }
 
 /*
@@ -26,31 +28,31 @@ Fasta::Fasta(string header, string sequence, int longueur, int position){
 ================================================================================
 */
 //GET
-string getHeader(){
-  return header;
-}
-string getSequence(){
-  return sequence;
-}
-int getLongueur(){
-  return longueur;
-}
-int getPos(){
-  return position;
-}
-//SET
-void setHeader(string header){
-  this->header=header;
-}
-void setSequence(string sequence){
-  this->sequence=sequence;
-}
-void setPos(int position){
-  this->position=position;
-}
-void setLongueur(int longueur){
-  this->position=position;
-}
+  string getHeader(){
+    return header;
+  }
+  string getSequence(){
+    return sequence;
+  }
+  int getLongueur(){
+    return longueur;
+  }
+  int getPos(){
+    return position;
+  }
+  //SET
+  void setHeader(string header){
+    this->header=header;
+  }
+  void setSequence(string sequence){
+    this->sequence=sequence;
+  }
+  void setPos(int position){
+    this->position=position;
+  }
+  void setLongueur(int longueur){
+    this->position=position;
+  }
 
 /*
 ================================================================================
@@ -59,13 +61,38 @@ void setLongueur(int longueur){
 */
 
 //Verifications
-bool isFasta() const{}
-bool isNucl(char c, bool degenerate=TRUE){}
-bool isBlank(char c);
+  bool isFasta(const string &fich, int argc) const{
+    ifstream fichier(fich.c_str());
+    if (argc != 2 && fichier){
+      return TRUE;
+    }
+    return FALSE;
+  }
+  bool isNucl(char c, bool degenerate=TRUE){}
+  bool isBlank(char c);
+
 
 //Autres
+
+  void extractionFasta(char *argv[]){
+    size_t compteur =0;
+    string contenu =" ";
+    ifstream fichier(argv[1], ios::in);
+    if (fichier){
+      while (getline(fichier, contenu)){
+        if (contenu[0] ==">"){
+          // Ajouter ici la partie du brouillon sur l'itération de classe
+        }
+      }
+    }
+  }
+
+/*
 vector<Fasta> extractionHeader(){}
 vector<Fasta> extractionHeader() const{}
 vector<Fasta> extractionSequence() const{}
 vector<Fasta> extractionPosition() const{}
 vector<Fasta> extractionLongueur() const{}
+*/
+
+}
